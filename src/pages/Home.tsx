8df2cee11d7070f70ef5ac37a3b5c429a6730c26
muvezwatanaka <muvezwatanaka@gmail.com>
2025-10-31 03:39:41 +0000

    fix: move keyframes to css file

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -128,15 +128,3 @@ export default function Home() {
     </div>
   );
 }
-
-@keyframes float {
-	0% {
-		transform: translatey(0px);
-	}
-	50% {
-		transform: translatey(-20px);
-	}
-	100% {
-		transform: translatey(0px);
-	}
-}
